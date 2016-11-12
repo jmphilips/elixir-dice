3fc34ecc46476b0b9fe5ed26399781b0edac771f
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-12 00:20:23 -0600

    Wrote some more documentation

--- lib/dice.ex
+++ lib/dice.ex
@@ -5,21 +5,27 @@ defmodule Dice do
     """
 
     @doc """
-        returns a list [1, 2, 3, 4, 5, 6]
+        Returns a list that has the value of each side of a six-sided die.
 
     ## Examples
-        iex()> Dice.create_dice
-            [1, 2, 3, 4, 5, 6]
+        iex> Dice.create_dice
+        [1, 2, 3, 4, 5, 6]
     """
     def create_dice do 
         [1, 2, 3, 4, 5, 6]
     end
 
+    @doc """
+        Takes the first number of a list.
+    """
     def roll_dice(dice) do
         [roll | _] = Enum.shuffle(dice)
         roll
     end
 
+    @doc """
+        Combines `create_dice` and `roll_dice` to get the value of the 'roll.'
+    """
     def rolled_number do 
         create_dice |> roll_dice   
     end
