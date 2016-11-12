bebbbdd9beb3a9115e3118f26dde70a63f2b6a93
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-11 22:30:42 -0600

    created a little recursive function

--- lib/dice.ex
+++ lib/dice.ex
@@ -9,9 +9,17 @@ defmodule Dice do
         roll
     end
 
-
     def rolled_number do 
         create_dice
         |> roll_dice
     end
+
+     def number_of_rolls(number) when number > 0 do
+        number + number_of_rolls(number - 1)
+    end
+
+    def number_of_rolls(number) do
+        number 
+    end
+
 end
