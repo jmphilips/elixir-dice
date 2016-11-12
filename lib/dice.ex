8bc4300805251a88dcd9b21cadf3b8955991480a
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-11 23:26:28 -0600

    finished the dice roll function and added benefits for rolling dice

--- lib/dice.ex
+++ lib/dice.ex
@@ -10,16 +10,23 @@ defmodule Dice do
     end
 
     def rolled_number do 
-        create_dice
-        |> roll_dice
+        create_dice |> roll_dice   
     end
 
-     def number_of_rolls(number) when number > 0 do
-        number + number_of_rolls(number - 1)
+    def number_of_rolls(number, old_list) when number == 0 do
+        old_list
     end
 
-    def number_of_rolls(number) do
-        number 
+    def number_of_rolls(number, old_list) do
+        number_of_rolls(number - 1, multiple_rolls(old_list))
+    end
+
+    def number_of_rolls(number) do 
+        number_of_rolls(number - 1, [rolled_number])
+    end
+
+    def multiple_rolls(old_list) do
+        List.flatten([rolled_number | old_list])
     end
 
 end
